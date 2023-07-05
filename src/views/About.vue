512d972e234ce4165efa2ee60448c230d73d8f68
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2023-07-05 19:50:34 -0400

    feat: improve color palette

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,36 +1,33 @@
 <template>
   <img src="../assets/undressed-woman.jpeg" alt="Undressed Woman" />
-  <main>
-    <section>
-      <h1>Become <span class="green">Undressed &trade;</span></h1>
-      <p>
-        Here, the unspoken finds its voice and inhibitions are left behind.
-        Immerse yourself in a world where
-        <span class="green">personal secrets take center stage</span>, shared
-        anonymously, and without judgment. Unveil your deepest desires, explore
-        untold stories, and embrace the <span class="green">freedom</span> to
-        express your innermost passions.
-      </p>
-      <div><a class="button" href="/">📣 Speak your truth</a></div>
-    </section>
-    <section>
-      <h1><span class="green">Naked</span> Truth</h1>
-      <p>
-        Haven't you noticed? This world is full of truth-loving liars! They
-        proclaim the value of <span class="green">honesty</span>, yet, force us
-        to stay silent about the things that define our very nature.
-      </p>
-      <br />
-      <p>
-        Being <span class="green">Undressed &trade;</span> is all about letting
-        yourself forget the harmful prejudices society has instilled in us and
-        re-learning to enjoy that forbidden fruit guilt-free. After all, it was
-        <span class="green">He</span> who planted that tree for us to enjoy 😉
-      </p>
-      <div><a class="button" href="/">🍎 Take a bite</a></div>
-    </section>
-    <br />
-  </main>
+  <section>
+    <h1>Become <span class="green">Undressed &trade;</span></h1>
+    <p>
+      Here, the unspoken finds its voice and inhibitions are left behind.
+      Immerse yourself in a world where
+      <span class="green">personal secrets take center stage</span>, shared
+      anonymously, and without judgment. Unveil your deepest desires, explore
+      untold stories, and embrace the <span class="green">freedom</span> to
+      express your innermost passions.
+    </p>
+    <div><a class="button" href="/">Speak your truth</a></div>
+  </section>
+  <section>
+    <h1><span class="green">Naked</span> Truth</h1>
+    <p>
+      Haven't you noticed? This world is full of truth-loving liars! They
+      proclaim the value of <span class="green">honesty</span>, yet, force us to
+      stay silent about the things that define our very nature.
+    </p>
+    <p>
+      Being <span class="green">Undressed &trade;</span> is all about letting
+      yourself forget the harmful prejudices society has instilled in us and
+      re-learning to enjoy that forbidden fruit guilt-free. After all, it was
+      <span class="green">He</span> who planted that tree for us to enjoy 😉
+    </p>
+    <div><a class="button" href="/">Take a bite</a></div>
+  </section>
+  <br />
 </template>
 
 <style scoped>
@@ -40,9 +37,9 @@ img {
   object-fit: cover;
 }
 
-main > section > h1 {
+section > h1 {
   font-size: 24px;
-  line-height: 4rem;
+  line-height: 3rem;
   font-weight: 700 !important;
   color: var(--color-heading);
 }
@@ -50,17 +47,20 @@ main > section > h1 {
 section {
   width: 90%;
   max-width: 650px;
-  margin: auto;
+  margin: 8px auto;
   color: var(--color-text);
+}
+section > p {
+  margin: 4px 0;
 }
 
 .button {
   display: inline-block;
-  background-color: var(--green);
+  background-color: var(--accent);
   text-decoration: none;
   color: var(--color-background);
   padding: 8px 16px;
-  font-size: 20px;
+  font-size: 18px;
   font-weight: 600;
   margin: 16px 0;
   border-radius: 8px;
